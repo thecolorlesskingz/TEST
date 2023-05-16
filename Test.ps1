6f24dc3bee5b91d315d79eec4320f3e3f1f8f950
thecolorlesskingz <101913351+thecolorlesskingz@users.noreply.github.com>
2023-05-16 16:58:12 -0400

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -9,9 +9,20 @@ $OSDModuleResource.OSDCloud.Default.Language = 'en-gb'
 #Customize the OSDCloud Values
 $OSDModuleResource.OSDCloud.Values.Activation = 'Retail'
 $OSDModuleResource.OSDCloud.Values.ReleaseID = '22H2','22H2'
-$OSDModuleResource.OSDCloud.Values.Edition = 'Home','Pro'
+$OSDModuleResource.OSDCloud.Values.Edition = 'Pro'
 $OSDModuleResource.OSDCloud.Values.Name = 'Windows 11 22H2 x64','Windows 11 21H2 x64','Windows 10 22H2 x64'
 $OSDModuleResource.OSDCloud.Values.Language = 'en-gb'
+
+#Customize the OSDCloudGUI Preferences
+$OSDModuleResource.StartOSDCloudGUI.ClearDiskConfirm = $false
+$OSDModuleResource.StartOSDCloudGUI.restartComputer = $false
+$OSDModuleResource.StartOSDCloudGUI.updateDiskDrivers = $true
+$OSDModuleResource.StartOSDCloudGUI.updateFirmware = $true
+$OSDModuleResource.StartOSDCloudGUI.updateSCSIDrivers = $true
+
+#Customize the OSDCloudGUI
+$OSDModuleResource.StartOSDCloudGUI.BrandName = 'PCSP'
+$OSDModuleResource.StartOSDCloudGUI.BrandColor = 'GREEN'
 
 #Start OSDCloud GUI
 Start-OSDCloudGUI
